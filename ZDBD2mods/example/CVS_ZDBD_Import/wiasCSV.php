a1d8080c7b31550f5a07fd9c811aea4dc3cef697
Paul Borchert <paul.borchert@gbv.de>
2018-01-10 16:32:19 +0000

    refactrue example

---
+++ ZDBD2mods/example/CVS_ZDBD_Import/wiasCSV.php
@@ -0,0 +1,25 @@
+#!/usr/bin/php
+<?php
+
+$CSVFILE=__DIR__."/Journals_WIAS.csv";
+
+$handle = fopen($CSVFILE, "r");
+if ($handle) {
+    echo '<?xml version="1.0" encoding="UTF-8"?> '."\n";
+    echo "<ZDBD2modsImport>\n";
+    while (($data = fgetcsv($handle, 0, ";")) !== false) {
+        echo '  <journal>'."\n";
+        echo '    <zdbdid>'.htmlspecialchars($data[1]).'</zdbdid>'."\n";
+        echo '  </journal>'."\n";
+        echo '  <journal>'."\n";
+        echo '    <zdbdid>'.$data[1].'</zdbdid>'."\n";
+        echo '  </journal>'."\n";
+    }
+    echo "</ZDBD2modsImport>"."\n";
+    fclose($handle);
+} else {
+    die ("Konnte Journals_WIAS.csv nicht öffnen.\n");
+}
+
+
+?>
